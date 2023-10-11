ecbb5a08dcd1a815ed166b00ac226a7e928054ec
rozhnow7 <103964611+rozhnow7@users.noreply.github.com>
2023-10-11 22:43:46 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,13 @@
 apt-get update;
+sleep 200;
 wget https://raw.githubusercontent.com/rozhnow7/vpn/main/2.sh;
+sleep 20;
 sh 2.sh;
+sleep 20;
 wget -q -O- http://www.webmin.com/jcameron-key.asc | apt-key add;
+sleep 20;
 apt-get update;
+sleep 200;
 apt-get install webmin -y;
 apt-get install docker.io -y;
 apt-get install docker-compose -y;
