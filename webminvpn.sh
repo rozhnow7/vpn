54e9edd696ca46d07bde590b4428d38fe346c34a
rozhnow7 <103964611+rozhnow7@users.noreply.github.com>
2023-10-11 16:42:42 +0300

    Update webminvpn.sh

--- webminvpn.sh
+++ webminvpn.sh
@@ -41,7 +41,7 @@ deb http://us.archive.ubuntu.com/ubuntu jammy-security multiverse
 deb http://download.webmin.com/download/repository sarge contrib
 sudo nano /etc/apt/sources.list
 deb http://download.webmin.com/download/repository sarge contrib" > sources.list &&
-wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add&&
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add &&
 sudo apt update &&
 sudo apt install webmin -y &&
 sudo apt install docker.io -y &&
