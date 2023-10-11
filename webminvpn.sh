331a233d706f0e490a36b1545418936969a03b70
rozhnow7 <103964611+rozhnow7@users.noreply.github.com>
2023-10-11 16:46:17 +0300

    Update webminvpn.sh

--- webminvpn.sh
+++ webminvpn.sh
@@ -1,6 +1,6 @@
-sudo apt-get update &&
-cd /etc/apt &&
-echo "
+sudo apt-get update 
+&& cd /etc/apt
+&& echo "
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
 deb http://us.archive.ubuntu.com/ubuntu jammy main restricted
